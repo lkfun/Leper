75ef2bade80595e67a8a2f195e91365e4bc9d4ea
lkfun <zuqiuzuqiu@126.com>
2025-08-01 16:36:08 +0800

    2025年8月1日

--- index.js
+++ index.js
@@ -1,17 +1,101 @@
 import fs from 'fs'
 import Config from './src/parser.js'
-// import download from './src/utils/download.js'
+ import download from './src/utils/download.js'
+ import readline from 'readline'
 
-// const configUrl = `${/** link */}/res/config/Config_w.sgs`
-const fileName = './dist/Config_w.sgs'
+
+ const configUrl = `https://web.sanguosha.com/10/pc/res/config/Config.sgs`
+const fileName = './Config.sgs'
 
 const configer = new Config()
-// await download(configUrl, fileName)
-// const data = await fs.promises.readFile(fileName)
-// configer.ParsePreloadConfig(data)
+ await download(configUrl, fileName)
+ const data = await fs.promises.readFile(fileName)
+ configer.ParsePreloadConfig(data)
+
+
+
 
 // const data = await fs.promises.readFile(fileName)
-fs.readFile(fileName, function (err, data) {
-  if (err) throw err
-  configer.ParsePreloadConfig(data)
-})
+// fs.readFile(fileName, function (err, data) {
+//   if (err) throw err
+//   configer.ParsePreloadConfig(data)
+// })
+
+
+async function findManagerInstances(filePath) {
+  const fileStream = fs.createReadStream(filePath, { encoding: 'utf8' });
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  const pattern = /return\s+([a-zA-Z_$][\w$]*Manager)\.GetInstance\(\);/g;
+  const matches = new Set();
+
+  for await (const line of rl) {
+    let match;
+    while ((match = pattern.exec(line)) !== null) {
+      matches.add(match[1]);
+    }
+  }
+
+  return Array.from(matches);
+}
+
+// // 使用示例
+// findManagerInstances('1209.txt')
+//   .then(results => console.log('找到的管理器:', results))
+//   .catch(console.error);
+
+// 开始解压----------
+// unZip.js:7
+// 解压文件结束------
+// unZip.js:19
+// Achievement.sgs
+// parser.js:125
+// ActivityAreaConfig.sgs
+// parser.js:125
+// ActivityViewConfig.sgs
+// parser.js:125
+// AdPushConfig.sgs
+// parser.js:125
+// AloneRideConfig.sgs
+// parser.js:125
+// AthleticsClientCardConfig.sgs
+// parser.js:125
+// AthleticsSeasonConf.sgs
+// parser.js:125
+// AthleticsSeasonRewardConf.sgs
+// parser.js:125
+// AuctionConfig.sgs
+// parser.js:125
+// AutoChessConfig.sgs
+// parser.js:125
+// BattleBgConfig.sgs
+// parser.js:125
+// BattleOfRelativesConfig.sgs
+// parser.js:125
+// BattlePassConfig.sgs
+// parser.js:125
+// BountyRaceConfig.sgs
+// parser.js:125
+// CalendarConfig.sgs
+// parser.js:125
+// Cards.sgs
+// parser.js:125
+// ChatFaceConfig.sgs
+// parser.js:125
+// ChessGeneralConfig.sgs
+// parser.js:125
+// CityConfig.sgs
+// parser.js:125
+// CityUnderSiegeConfig.sgs
+// parser.js:125
+// ClientHolidayEffectConf.sgs
+// parser.js:125
+// ClientXiaoShaServiceConf.sgs
+// parser.js:125
+// CommonActivityConfig.sgs
+// parser.js:125
+// CustomerCenter.sgs
+// parser.js:125
--- src/parser.js
+++ src/parser.js
@@ -3,7 +3,11 @@ import zlib from 'zlib'
 import CtrUtil from './utils/CtrUtil.js'
 // import { gunzipSync } from './utils/zlib.cjs'
 import fs from 'fs'
-import unZip from './utils/unZip.js'
+import unZip from './utils/unZip.js' 
+import sqlite3 from 'sqlite3';
+import {pinyin} from 'pinyin-pro'
+
+
 
 export default class Config {
   constructor() {
@@ -19,7 +23,7 @@ export default class Config {
   preloadConfigerList = []
   initConfigerList() {
     this.preloadConfigerList.length = 0
-    this.preloadConfigerList = ['gn_dbs_quest', 'ff_exchange_new', 'ff_dbs_lottery_new', 'sys_treasure_chest']
+    this.preloadConfigerList = ['Cards','GeneralCards','GeneralSkinInfoConfig','GeneralSkinSuitConfig','GameSoundConfig','GameRuleConfig','GameGeneralConfig','GameModeConfig','GameSpriteConfig']//, 'ff_exchange_new', 'ff_dbs_lottery_new', 'sys_treasure_chest']
   }
 
   ParsePreloadConfig(data) {
@@ -45,6 +49,63 @@ export default class Config {
     for (let i = 0; i < length; i++) {
       let configName = this.preloadConfigerList[i]
       let data = this.WorkParse(this.getZipConfigData(configName))
+      //console.log("data")
+      //console.log(data)
+
+      if(configName=="GeneralCards"){
+        var GeneralCardsdata=`INSERT OR REPLACE INTO "main"."wujiang"("CharacterId", "Name", "pinyin") VALUES`
+        var dataJson=JSON.parse(data)
+        var Characterlist=dataJson["Character"]
+        for (let j =0;j<Characterlist.length;j++){
+          if(Characterlist[j]["Name"]){
+            GeneralCardsdata+=` ('${Characterlist[j]["CharacterId"]}', '${Characterlist[j]["Name"]}', '${pinyin(Characterlist[j]["Name"])}'),`
+          }
+        }
+        GeneralCardsdata=GeneralCardsdata.substring(0,GeneralCardsdata.length-1)+";"
+        fs.writeFileSync(`./dist/config/${configName}.sql`, GeneralCardsdata)
+
+        // 创建并连接到数据库（如果不存在，将创建一个名为 mydatabase.db 的文件）
+        let db = new sqlite3.Database('D:/shenglvdata/NEWSHENGLV.DB.2025', (err) => {
+          if (err) {
+            console.error('Error opening database ' + err.message);
+          } else {
+            console.log('Connected to the SQLite database.');
+          }
+        });
+
+        // 执行 SQL 语句
+        db.serialize(() => {
+          db.run(GeneralCardsdata);
+        });
+
+        // 关闭数据库连接
+        db.close((err) => {
+          if (err) {
+            console.error('Error closing database ' + err.message);
+          } else {
+            console.log('Closed the database connection.');
+          }
+        });
+
+      }
+      if(configName=="GeneralSkinInfoConfig"){
+        var alist=[]
+        var dataJson=JSON.parse(data)
+        var SkinConflist=dataJson["SkinConf"]
+        var ClientAwakeSkinlist=dataJson["ClientAwakeSkin"]
+        var SkinSpineConflist=dataJson["SkinSpineConf"]
+        for (let i=0;i<SkinConflist.length;i++){
+          alist.push(SkinConflist[i]["SkinID"])
+        }
+        for (let i=0;i<ClientAwakeSkinlist.length;i++){
+          alist.push(ClientAwakeSkinlist[i]["SkinID"])
+        }
+        for (let i=0;i<SkinSpineConflist.length;i++){
+          alist.push(SkinSpineConflist[i]["SkinID"])
+        }
+        fs.writeFileSync(`./dist/config/${configName}.data`, alist.toString())
+        console.log(`解析配置${configName}`)
+      }
       try {
         fs.writeFileSync(`./dist/config/${configName}.json`, data)
         console.log(`解析配置${configName}`)
@@ -61,6 +122,7 @@ export default class Config {
       let len = this.zipFiles.length
 
       for (let i = 0; i < len; i++) {
+        console.log(this.zipFiles[i].name)
         if (this.zipFiles[i].name == name) {
           return this.zipFiles[i].data
         }
@@ -70,6 +132,8 @@ export default class Config {
     console.log('找不到配置文件：', configName)
     return null
   }
+
+
 
   /**
    *
